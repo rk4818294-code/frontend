1e0e3d7053a8c1551d2b9dcd11a37576f538f8e6
Rohit kumar <rk4818294@gmail.com>
2026-03-11 21:07:45 +0530

    msg

---
+++ JAVA SCRIPT/Mse-ques.js
@@ -0,0 +1,49 @@
+// MSE SEM-2
+
+// frontend important questions:
+// q1-10m:-create bootstrap grid which have two responsive carts that appear side by side on large screen and stack vertically on small screen?
+// q2- explain git commands to merge one branch into another branch and explain the purpose of merge operation?
+// q3- create responsive webpage using grid that display 3 carts in a row on a large screen and 1 cart per row on  large screen?
+// q4- git command to create ,switch merge into main branch
+// q4 create a git command to clone a repository ,
+
+// 6m
+// q1- build html code to create a responsive card using bootstrap with image title and description
+// q2- explain the purpose of git ignore file using examples
+// q3- write steps to clone a repository from GitHub and list repository files?
+// q4- write bootstrap code to create a navigation bar with name about contact service?
+
+// git clone repository link.
+
+
+
+
+
+// Imp Question :
+
+// 10 marks >>>>>>>>>>>>>>
+
+// Create bootstrap grid which have 2 responsive card that appears side by side on large screen and stack vertically on small screen.
+// Explain git commands to merge one branch to another branch and also explain the merge operation.
+// Create responsive web page using grid that display 3 cards in a row on large screen and 1 card per row in small screen.
+// Git command to create a branch, switch the branch, merge into main, push to GitHub, and explain the concept of branching.
+// Command to clone a repo, create a new file, commit the changes, push them to repo.
+
+// 6 marks >>>>>>>>>>>>>>
+// Build HTML code to create a responsive card with image, title, and description with an example.
+// Explain the purpose of .gitignore file with an example.
+// Write steps to clone a repo from GitHub and list repo files.
+// Write a Bootstrap code to create a navbar and link Home, Contact, Service.
+
+
+
+
+
+
+
+
+
+
+
+
+
