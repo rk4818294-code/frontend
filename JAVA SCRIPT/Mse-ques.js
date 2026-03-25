d9a570710df10724762d60f7a4a2ffb4fb550e29
Rohit kumar <rk4818294@gmail.com>
2026-03-25 21:43:37 +0530

    msg

--- JAVA SCRIPT/Mse-ques.js
+++ JAVA SCRIPT/Mse-ques.js
@@ -2,16 +2,18 @@
 
 // frontend important questions:
 // 10m:-
-// create bootstrap grid which have 2 responsive carts that appear side by side on large screen and stack vertically on small screen?
+// create bootstrap grid which have 2 responsive carts that appear side by side on large screen and stack vertically on small screen?          DONE
 //  explain git commands to merge one branch into another branch and explain the purpose of merge operation?
-//  create responsive webpage using grid that display 3 carts in a row on a large screen and 1 cart per row on  large screen?
+//  create responsive webpage using grid that display 3 cards in a row on a large screen and 1 card per row on  small screen?
 //  git command to create branch  ,switch merge into main branch
 //  create a git command to clone a repository ,
-// Create responsive web page using grid that display 3 cards in a row on large screen and 1 card per row in small screen.
 // Git command to create a branch, switch the branch, merge into main, push to GitHub, and explain the concept of branching.
 // Command to clone a repo, create a new file, commit the changes, push them to repo.
 
+
+
 // 6m
+
 // Build HTML code to create a responsive card with image, title, and description with an example.
 // Explain the purpose of git ignore file with an example.
 // q3- write steps to clone a repository from GitHub and list repository files?
